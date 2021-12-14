453536c1e79066c542e111b203f70786cef60deb
kpointnotice <kpointnotice@gmail.com>
2021-12-14 11:07:50 +0900

    환경설정 수정

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 //기본 설정
 const express = require('express');
-const app = express();
-const PORT = 3456;
+const https = require('https');
+const fs = require('fs');
+const dotenv = require('dotenv');
 
-//정적 파일
-// app.use(express.static(__dirname + '/public'));
+const app = express();
+dotenv.config();
+
+//ssl 설정
+const options = {
+    key: fs.readFileSync(process.env.SSL_KEY),
+    cert: fs.readFileSync(process.env.SSL_CERT),
+    ca: fs.readFileSync(process.env.SSL_CA),
+    passphrase: process.env.SSL_PASSPHRASE
+  }
 
 //라우팅
 app.get('/p2p', (req, res) => {
@@ -12,6 +21,6 @@ app.get('/p2p', (req, res) => {
 })
 
 //서버 실행
-app.listen(PORT, () => {
-    console.log(`Listen : ${PORT}`);
+https.createServer(options, app).listen(process.env.PORT, () => {
+    console.log(`Listen : ${process.env.PORT}`);
 })
